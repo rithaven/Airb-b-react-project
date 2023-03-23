c84a1e71b596c6a3bfa928f204101595df66dfc8
rithaven <veneruth4@gmail.com>
2023-03-23 15:06:03 +0200

    Remove react from imports

---
+++ airbnb/src/App.jsx
@@ -0,0 +1,30 @@
+
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <div className="p-4">
+                <Card
+                key={item.id}
+                {...item}
+            />
+            </div>
+            
+        )
+    })        
+    
+          
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="flex">
+                {cards}
+            </section>
+        </div>
+    )
+}
---
+++ airbnb/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="flex h-16 py-5 shadow-2xl px-9">
+        <img
+          className="h-8"
+          src="/images/logo.png"
+        />
+      </nav>
+    </div>
+  );
+}
